95776e148abbc8966a7a27d6731b333be9a3a4e2
alongoldboim <alongoldboim@gmail.com>
2016-03-24 20:40:27 +0200

    adding models controller and needed migrations
    Changing classification to tags
    
    Adding authentication
    
    Replaced api call to direct call to automation engine
    
    Adding service object methods
    
    Refactoring
    
    Rename deployment model to container-deployment
    
    Moving methods from controller to model
    
    Added nfs support
    
    Added support for arrays input for ldap auth
    
    Changed template to use heredoc
    
    More refactoring
    
    Adding ansbile labels for nodes
    
    removed migration to diff pr
    
    removed service models to seprate pr
    
    Changes made by ui work
    
    Change controller to api controller
    
    Adding tests
    
    New ui changes
    
    Changed to fit api desc
    
    removing api from pr
    
    refactoring class

--- app/models/container_deployment_node.rb
+++ app/models/container_deployment_node.rb
@@ -4,4 +4,25 @@ class ContainerDeploymentNode < ApplicationRecord
   serialize :labels, Hash
   serialize :customizations, Hash
   acts_as_miq_taggable
+
+  def node_address
+    if vm
+      vm.hostnames.last || vm.hardware.ipaddresses.last
+    elsif address
+      address
+    end
+  end
+
+  def roles
+    tags.reset
+    roles = tags.collect { |tag| tag.name.gsub("/user/", "").gsub("deployment_master", "master") }.uniq
+    roles
+  end
+
+  def to_ansible_config_format
+    {
+      "connect_to" => node_address,
+      "roles"      => roles
+    }
+  end
 end
